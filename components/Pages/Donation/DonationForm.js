80b6aec7519888c4d6464ddcb25206b6541daa3f
Alex Hernandez <vlexhndz@gmail.com>
2021-04-10 22:45:44 +0200

    efwawfe

--- components/Pages/Donation/DonationForm.js
+++ components/Pages/Donation/DonationForm.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const DonationForm = () => {
   return (
-    <main className={`lg:col-span-8 xl:col-span-6 xl:col-start-3`}>Hola</main>
+    <div className={`lg:col-span-8 xl:col-span-6 xl:col-start-3`}>
+      <div className="bg-white dark:bg-gray-700 px-4 py-6 shadow sm:p-6 sm:rounded-lg cursor-pointer"></div>
+    </div>
   );
 };
 
--- pages/donation/[donation].js
+++ pages/donation/[donation].js
@@ -4,14 +4,41 @@ import UserCard from "components/Pages/Profile/UserCard";
 import { DONATION_PAGE } from "pages";
 import LeftSidebar from "components/Pages/User/LeftSidebar";
 import DonationForm from "components/Pages/Donation/DonationForm";
+import { useRouter } from "next/router";
 
 export default function Donation() {
   const page = DONATION_PAGE;
+  const router = useRouter();
+  const handleGoBack = () => {
+    router.back();
+  };
   return (
     <Layout>
       <div className="py-10">
         <div className="max-w-3xl mx-auto sm:px-6 flex flex-col lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
           <UserCard mobile page={page} profile />
+          <div className="text-gray-500 dark:text-gray-300 text-sm col-span-2 hidden xl:block">
+            <span
+              className="flex items-center cursor-pointer"
+              onClick={handleGoBack}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>{" "}
+              Go back
+            </span>
+          </div>
           <DonationForm />
           {/* <Feed page={page} /> */}
           <UserCard page={page} />
