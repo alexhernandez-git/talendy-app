cc0e52b11cdbd04331bd291e31337616f9971e27
Alex Hernandez <vlexhndz@gmail.com>
2021-04-11 16:15:54 +0200

    feafewa

--- components/Pages/Donation/DonationForm.js
+++ components/Pages/Donation/DonationForm.js
@@ -365,33 +365,15 @@ const DonationForm = () => {
                 </div>
               </dd>
             </div>
-            <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 items-center">
-              <dt className="text-sm font-medium text-gray-500">
-                Email address
-              </dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                margotfoster@example.com
-              </dd>
-            </div>
-            <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 items-center">
-              <dt className="text-sm font-medium text-gray-500">
-                Salary expectation
-              </dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                $120,000
-              </dd>
-            </div>
-            <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-              <dt className="text-sm font-medium text-gray-500">About</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-                incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-                consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                proident. Irure nostrud pariatur mollit ad adipisicing
-                reprehenderit deserunt qui eu.
-              </dd>
-            </div>
           </dl>
+        </div>
+        <div class="pb-6">
+          <button
+            type="button"
+            className="w-full uppercase bg-gradient-to-r from-green-400 to-green-600 hover:to-green-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white"
+          >
+            DONATE NOW
+          </button>
         </div>
       </div>
     </div>
