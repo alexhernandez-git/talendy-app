4d08d43752885191cd8254c6e75cd64acc2d46bb
Alex Hernandez <vlexhndz@gmail.com>
2021-04-20 16:48:00 +0200

    waeffawe

--- pages/profile/mynetwork/connections.js
+++ pages/profile/mynetwork/connections.js
@@ -5,6 +5,8 @@ import { CONNECTIONS_PAGE } from "pages";
 import LeftSidebar from "components/Pages/MyNetwork/LeftSidebar";
 import useAuthRequired from "hooks/useAuthRequired";
 import Spinner from "components/Layout/Spinner";
+import ProfileMenu from "components/Pages/Profile/ProfileMenu";
+import User from "components/Layout/User";
 export default function Posts() {
   const page = CONNECTIONS_PAGE;
   const [canRender, authReducer, initialDataFetched] = useAuthRequired(page);
@@ -18,7 +20,19 @@ export default function Posts() {
         <div className="max-w-3xl mx-auto sm:px-6 flex flex-col lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
           <LeftSidebar page={page} />
           <UserCard mobile page={page} profile user={authReducer.user} />
-          <Feed page={page} profile />
+          <main className={`lg:col-span-8 xl:col-span-6 xl:col-start-3`}>
+            <ProfileMenu page={page} />
+            <div className="mt-4">
+              <ul className="space-y-4">
+                <li>
+                  <User page={page} />
+                </li>
+                <li>
+                  <User page={page} />
+                </li>
+              </ul>
+            </div>
+          </main>
           <UserCard page={page} profile user={authReducer.user} />
         </div>
       </div>
