3ef50431aa549e941fe54986689edaeb67db29e1
Alex Hernandez <vlexhndz@gmail.com>
2021-04-26 21:10:03 +0200

    fweawfe

--- hooks/useGetNotification.js
+++ hooks/useGetNotification.js
@@ -6,6 +6,31 @@ const useGetNotification = (notification) => {
   console.log(notification);
   let connection;
   switch (notification.type) {
+    case "ME":
+      const many_messages = notification.messages.length > 1;
+      let message = "";
+      if (notification.actor) {
+        message = `${
+          many_messages
+            ? notification.messages.length + " messages"
+            : "New message"
+        } from ${notification.actor.username}`;
+      } else {
+        message = `${
+          many_messages
+            ? notification.messages.length + " messages"
+            : "New message"
+        }`;
+      }
+      if (notification.messages.length > 0 && !many_messages) {
+        message = notification.messages[0].text;
+      }
+      return {
+        event_message: `New message`,
+        message: message,
+        user: notification.actor,
+      };
+      break;
     case "NC":
       connection = notification.connection;
       const connectedUser =
