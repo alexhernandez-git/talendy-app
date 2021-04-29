84a6a347a013fee5fe3f8027ddd7e5a28a541504
Alex Hernandez <vlexhndz@gmail.com>
2021-04-29 15:19:22 +0200

    werfafwe

--- components/Layout/CreateEditPostModal.js
+++ components/Layout/CreateEditPostModal.js
@@ -370,7 +370,7 @@ const CreateEditPostModal = ({
                         <button
                           type="button"
                           onMouseDown={handleToggleCommunities}
-                          className="text-gray-500 dark:text-gray-100 bg-white dark:bg-gray-700 relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
+                          className="text-gray-500 cursor-pointer dark:text-gray-100 bg-white dark:bg-gray-700 relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left sm:text-sm"
                           aria-haspopup="listbox"
                           aria-expanded="true"
                           aria-labelledby="listbox-label"
@@ -428,7 +428,7 @@ const CreateEditPostModal = ({
                               </li>
 
                               <li
-                                className="text-gray-500 dark:text-gray-100 cursor-pointer select-none relative py-2 pl-3 pr-9"
+                                className="text-gray-500 dark:text-gray-100 cursor-pointer select-none relative py-2 pl-3 pr-9 hover:bg-gray-100 dark:hover:bg-gray-900"
                                 id="listbox-option-0"
                                 role="option"
                               >
