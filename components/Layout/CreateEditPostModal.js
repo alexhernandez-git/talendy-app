972f196447e5344c78c81c60a3394c5776c059cf
Alex Hernandez <vlexhndz@gmail.com>
2021-04-29 15:00:09 +0200

    wefaawe

--- components/Layout/CreateEditPostModal.js
+++ components/Layout/CreateEditPostModal.js
@@ -59,6 +59,21 @@ const CreateEditPostModal = ({
   const handleCloseImages = () => {
     setImagesOpen(false);
   };
+  const [communitiesOpen, setCommunitiesOpen] = useState(false);
+  const handleOpenCommunities = () => {
+    setCommunitiesOpen(true);
+  };
+  const handleCloseCommunities = () => {
+    if (communitiesOpen) {
+      setCommunitiesOpen(false);
+    }
+  };
+  const handleToggleCommunities = () => {
+    setCommunitiesOpen(!communitiesOpen);
+  };
+  const communitiesRef = useRef();
+  useOutsideClick(communitiesRef, () => handleCloseCommunities());
+
   return (
     <div
       className={`${
@@ -342,7 +357,92 @@ const CreateEditPostModal = ({
                     <Editor postForm />
                   </dd>
                 </div>
+                <div className="sm:col-span-1">
+                  <dd className="text-sm text-gray-900">
+                    <div>
+                      <label
+                        id="listbox-label"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                      >
+                        Community
+                      </label>
+                      <div className="mt-2 relative">
+                        <button
+                          type="button"
+                          onMouseDown={handleToggleCommunities}
+                          className="text-gray-500 dark:text-gray-100 bg-white dark:bg-gray-700 relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
+                          aria-haspopup="listbox"
+                          aria-expanded="true"
+                          aria-labelledby="listbox-label"
+                        >
+                          <span className="block truncate">Development</span>
+                          <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                            <svg
+                              className="h-5 w-5 text-gray-400"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                clip-rule="evenodd"
+                              />
+                            </svg>
+                          </span>
+                        </button>
+                        <div ref={communitiesRef}>
+                          {communitiesOpen && (
+                            <ul
+                              className=" absolute z-10 mt-1 w-full bg-white dark:bg-gray-800 shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                              tabindex="-1"
+                              role="listbox"
+                              aria-labelledby="listbox-label"
+                              aria-activedescendant="listbox-option-3"
+                            >
+                              <li
+                                className="text-gray-500 dark:text-gray-100 cursor-default select-none relative py-2 pl-3 pr-9"
+                                id="listbox-option-0"
+                                role="option"
+                              >
+                                <span className="font-normal block truncate">
+                                  Development
+                                </span>
 
+                                <span className="text-orange-600 absolute inset-y-0 right-0 flex items-center pr-4">
+                                  <svg
+                                    className="h-5 w-5"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                      clipRule="evenodd"
+                                    />
+                                  </svg>
+                                </span>
+                              </li>
+
+                              <li
+                                className="text-gray-500 dark:text-gray-100 cursor-pointer select-none relative py-2 pl-3 pr-9"
+                                id="listbox-option-0"
+                                role="option"
+                              >
+                                <span className="font-normal block truncate">
+                                  Business
+                                </span>
+                              </li>
+                            </ul>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </dd>
+                </div>
                 {!imagesOpen && (
                   <div className="mt-2 sm:col-span-2">
                     <button
