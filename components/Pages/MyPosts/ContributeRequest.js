fe86f3fad7b8198990e8c3814bfeb373478ff8f1
Alex Hernandez <vlexhndz@gmail.com>
2021-05-07 12:42:36 +0200

    faweewfa

--- components/Pages/MyPosts/ContributeRequest.js
+++ components/Pages/MyPosts/ContributeRequest.js
@@ -58,7 +58,7 @@ const ContributeRequest = ({ contribute_request }) => {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              34000 Karma
+              {contribute_request?.user?.karma_amount} Karma
             </p>
           </div>
           <div className="flex-shrink-0">
@@ -78,14 +78,13 @@ const ContributeRequest = ({ contribute_request }) => {
             </button>
           </div>
         </div>
-        <div className="mt-2 text-sm text-gray-700  dark:text-gray-100 space-y-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-          deleniti, commodi itaque aliquid doloribus illum dolore aut nisi
-          minus, sint eligendi harum veniam totam, iure modi aliquam dicta unde
-          quo?
-        </div>
+        {contribute_request?.reason && (
+          <div className="mt-2 text-sm text-gray-700  dark:text-gray-100 space-y-4">
+            {contribute_request.reason}
+          </div>
+        )}
         <div className="border sm:rounded-lg mt-4">
-          <Post />
+          <Post post={contribute_request.post} />
         </div>
       </div>
     </li>
