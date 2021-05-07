2bc83c41fb0b86b0151cb23f902b929235e29452
Alex Hernandez <vlexhndz@gmail.com>
2021-05-07 15:40:10 +0200

    fewraf

--- components/Pages/User/UserMenu.js
+++ components/Pages/User/UserMenu.js
@@ -7,21 +7,25 @@ import {
   USER_CONTRIBUTED_POSTS_PAGE,
 } from "pages";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const UserMenu = ({ page }) => {
+  const [menuValue, setMenuValue] = useState(page);
   const router = useRouter();
   const userId = router.query?.user;
   const handleChangeMobileMenu = (e) => {
     e.preventDefault();
     console.log(e.target.value);
+
     switch (e.target.value) {
-      case "PO":
-        router.push(`/user/${userId}`);
-        break;
-      case "CO":
+      case USER_POSTS_PAGE:
         router.push(`/user/contributed/${userId}`);
         break;
+      case USER_CONTRIBUTED_POSTS_PAGE:
+        router.push(`/user/${userId}`);
+        break;
     }
+    setMenuValue(e.target.value);
   };
   return (
     <div className="px-4 mb-4 sm:px-0">
@@ -31,11 +35,12 @@ const UserMenu = ({ page }) => {
         </label>
         <select
           onChange={handleChangeMobileMenu}
+          value={menuValue}
           id="question-tabs"
           className="block w-full rounded-3xl border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-orange-500 focus:ring-orange-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-100"
         >
-          <option value="PO">Posts</option>
-          <option value="CO">Contributed</option>
+          <option value={USER_POSTS_PAGE}>Posts</option>
+          <option value={USER_CONTRIBUTED_POSTS_PAGE}>Contributed</option>
         </select>
       </div>
       <div className="hidden sm:block">
