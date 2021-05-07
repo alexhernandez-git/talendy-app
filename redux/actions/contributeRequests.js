a5ec0cc7af0b9cf801968d71aac7511934e6a51a
Alex Hernandez <vlexhndz@gmail.com>
2021-05-07 18:17:48 +0200

    fweawfe

--- redux/actions/contributeRequests.js
+++ redux/actions/contributeRequests.js
@@ -10,6 +10,12 @@ import {
   FETCH_CONTRIBUTE_REQUEST,
   FETCH_CONTRIBUTE_REQUEST_SUCCESS,
   FETCH_CONTRIBUTE_REQUEST_FAIL,
+  ACCEPT_CONTRIBUTE_REQUEST,
+  ACCEPT_CONTRIBUTE_REQUEST_SUCCESS,
+  ACCEPT_CONTRIBUTE_REQUEST_FAIL,
+  IGNORE_CONTRIBUTE_REQUEST,
+  IGNORE_CONTRIBUTE_REQUEST_SUCCESS,
+  IGNORE_CONTRIBUTE_REQUEST_FAIL,
 } from "../types";
 
 import { createAlert } from "./alerts";
@@ -86,4 +92,64 @@ export const fetchMoreContributeRequests = () => async (dispatch, getState) => {
         });
       });
   }
+};
+
+export const acceptContributeRequest = (contribute_request__pk) => async (
+  dispatch,
+  getState
+) => {
+  await dispatch({
+    type: ACCEPT_CONTRIBUTE_REQUEST,
+  });
+  await axios
+    .get(
+      `${process.env.HOST}/api/contribute-requests/${contribute_request__pk}/accept/`,
+      tokenConfig(getState)
+    )
+    .then(async (res) => {
+      try {
+        await dispatch({
+          type: ACCEPT_CONTRIBUTE_REQUEST_SUCCESS,
+          payload: contribute_request__pk,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    .catch(async (err) => {
+      await dispatch({
+        type: ACCEPT_CONTRIBUTE_REQUEST_FAIL,
+        payload: { data: err.response?.data, status: err.response?.status },
+      });
+    });
+};
+
+export const ignoreContributeRequest = (contribute_request__pk) => async (
+  dispatch,
+  getState
+) => {
+  await dispatch({
+    type: IGNORE_CONTRIBUTE_REQUEST,
+  });
+  await axios
+    .delete(
+      `${process.env.HOST}/api/contribute-requests/${contribute_request__pk}/`,
+      tokenConfig(getState)
+    )
+    .then(async (res) => {
+      try {
+        await dispatch({
+          type: IGNORE_CONTRIBUTE_REQUEST_SUCCESS,
+          payload: contribute_request__pk,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    .catch(async (err) => {
+      await dispatch({
+        type: IGNORE_CONTRIBUTE_REQUEST_FAIL,
+        payload: { data: err.response?.data, status: err.response?.status },
+      });
+    });
 };
