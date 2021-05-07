8f9067ceaadd32a79808ea0baca95b51c0b85e51
Alex Hernandez <vlexhndz@gmail.com>
2021-05-07 15:43:04 +0200

    fweaf

--- components/Pages/Search/SearchMenu.js
+++ components/Pages/Search/SearchMenu.js
@@ -2,11 +2,27 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { SEARCH_USERS_PAGE, SEARCH_POSTS_PAGE } from "pages";
 import React from "react";
+import { useState } from "react";
 
 const SearchMenu = ({ page }) => {
   const router = useRouter();
   const search =
     router?.query?.search?.length > 0 ? router.query.search[0] : "";
+  const [menuValue, setMenuValue] = useState(page);
+  const handleChangeMobileMenu = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+
+    switch (e.target.value) {
+      case SEARCH_POSTS_PAGE:
+        router.push(`/search/${search}`);
+        break;
+      case SEARCH_USERS_PAGE:
+        router.push(`/search/users/${search}`);
+        break;
+    }
+    setMenuValue(e.target.value);
+  };
   return (
     <div className="px-4 mb-4 sm:px-0">
       <div className="sm:hidden">
@@ -14,12 +30,14 @@ const SearchMenu = ({ page }) => {
           Select a tab
         </label>
         <select
+          onChange={handleChangeMobileMenu}
+          value={menuValue}
           id="question-tabs"
           className="block w-full rounded-3xl border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-orange-500 focus:ring-orange-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-100"
         >
-          <option>Posts</option>
+          <option value={SEARCH_POSTS_PAGE}>Posts</option>
 
-          <option>Users</option>
+          <option value={SEARCH_USERS_PAGE}>Users</option>
         </select>
       </div>
       <div className="hidden sm:block">
