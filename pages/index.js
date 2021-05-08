13b45ae7f6f49751e4bf376fa8d99a10f86e70b9
Alex Hernandez <vlexhndz@gmail.com>
2021-05-08 17:19:53 +0200

    few

--- pages/index.js
+++ pages/index.js
@@ -262,17 +262,18 @@ export default function Home() {
                   src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
                   alt="People working on laptops"
                 />
-                <div className="absolute inset-0 bg-gradient-to-r from-pink-600 to-orange-600 mix-blend-multiply opacity-80" />
+                <div className="absolute inset-0 bg-gradient-to-r from-pink-600 to-orange-600 mix-blend-multiply opacity-90" />
               </div>
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                  <span className="block text-white">Welcome to the best</span>
+                  <span className="block text-white ">Welcome to the best</span>
                   <span className="block text-orange-200">
                     collaboration community
                   </span>
                 </h1>
                 <p className="mt-6 max-w-lg mx-auto text-center text-xl text-orange-200 sm:max-w-3xl">
-                  Make contacts, improve your skills helping people.
+                  Make contacts, improve your skills and get reputation helping
+                  people.
                 </p>
                 <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                   <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
