5c27e11f6f9aa401d8e5746e44646e9579a3af53
Alex Hernandez <vlexhndz@gmail.com>
2021-05-11 17:15:35 +0200

    fwaee

--- components/Pages/ContributeRoom/Message.js
+++ components/Pages/ContributeRoom/Message.js
@@ -4,7 +4,20 @@ import { useSelector } from "react-redux";
 
 const Message = ({ message, myMessage }) => {
   console.log(message);
-
+  const roomMessagesReducer = useSelector((state) => state.roomMessagesReducer);
+  const messageIndex = roomMessagesReducer.messages.results.indexOf(message);
+  const previous_message =
+    roomMessagesReducer.messages.results[messageIndex - 1];
+  const next_message = roomMessagesReducer.messages.results[messageIndex + 1];
+  let previous_same_user_created = false;
+  if (previous_message) {
+    previous_same_user_created =
+      previous_message?.sent_by?.id === message?.sent_by?.id;
+  }
+  let next_same_user_created = false;
+  if (next_message) {
+    next_same_user_created = next_message?.sent_by?.id === message?.sent_by?.id;
+  }
   return (
     <li className="">
       <div className={`${myMessage ? "float-right " : "float-left "}  w-11/12`}>
@@ -14,38 +27,44 @@ const Message = ({ message, myMessage }) => {
           }`}
         >
           {!myMessage && (
-            <a
-              className="flex-shrink-0 mr-3 cursor-pointer"
-              href={`/user/${message?.sent_by?.id}`}
-              target="_blank"
-            >
-              {message?.sent_by?.picture ? (
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src={
-                    new RegExp(
-                      `${process.env.HOST}|https://freelanium.s3.amazonaws.com`
-                    ).test(message?.sent_by?.picture)
-                      ? message?.sent_by?.picture
-                      : process.env.HOST + message?.sent_by?.picture
-                  }
-                  alt=""
-                ></img>
+            <>
+              {!next_same_user_created ? (
+                <a
+                  className="flex-shrink-0 mr-3 cursor-pointer"
+                  href={`/user/${message?.sent_by?.id}`}
+                  target="_blank"
+                >
+                  {message?.sent_by?.picture ? (
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={
+                        new RegExp(
+                          `${process.env.HOST}|https://freelanium.s3.amazonaws.com`
+                        ).test(message?.sent_by?.picture)
+                          ? message?.sent_by?.picture
+                          : process.env.HOST + message?.sent_by?.picture
+                      }
+                      alt=""
+                    ></img>
+                  ) : (
+                    <span className="bg-gray-100 rounded-full overflow-hidden h-10 w-10">
+                      <svg
+                        className="text-gray-300 bg-gray-100 rounded-full h-10 w-10"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                      </svg>
+                    </span>
+                  )}
+                </a>
               ) : (
-                <span className="bg-gray-100 rounded-full overflow-hidden h-10 w-10">
-                  <svg
-                    className="text-gray-300 bg-gray-100 rounded-full h-10 w-10"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
+                <div className="w-14 block"></div>
               )}
-            </a>
+            </>
           )}
           <div className="w-full mt-4 ">
-            {!myMessage && (
+            {!myMessage && !next_same_user_created && (
               <div className="mb-2">
                 <a
                   href={`/user/${message?.sent_by?.id}`}
