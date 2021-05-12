93f172cf9e661becf9db574221d6df8eec8ccbab
Alex Hernandez <vlexhndz@gmail.com>
2021-05-12 23:11:09 +0200

    wafewef

--- components/Pages/Contribute/ContributeWhiteboard.js
+++ components/Pages/Contribute/ContributeWhiteboard.js
@@ -9,175 +9,130 @@ import ContributeRoomChatEditor from "components/Editor/ContributeRoomChatEditor
 import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
-
+import useCanvas from "hooks/useCanvas";
 const ContributeWhiteboard = ({ socketRef }) => {
   const dispatch = useDispatch();
   const authReducer = useSelector((state) => state.authReducer);
+  const canvasRef = useCanvas(([canvas, ctx]) => {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    var colors = document.getElementsByClassName("color");
+    var ctx = canvas.getContext("2d");
 
-  const canvasRef = useRef();
-  const [canvas, setCanvas] = useState();
-  const [colors, setColors] = useState();
-  const [context, setContext] = useState();
-  const [firstLoad, setFirstLoad] = useState(true);
-  const [drawing, setDrawing] = useState(false);
-  const [current, setCurrent] = useState({ color: "black" });
-  useEffect(() => {
+    var current = {
+      color: "black",
+    };
+    var drawing = false;
+
+    canvas.addEventListener("mousedown", onMouseDown, false);
+    canvas.addEventListener("mouseup", onMouseUp, false);
+    canvas.addEventListener("mouseout", onMouseUp, false);
+    canvas.addEventListener("mousemove", throttle(onMouseMove, 10), false);
+
+    //Touch support for mobile devices
+    canvas.addEventListener("touchstart", onMouseDown, false);
+    canvas.addEventListener("touchend", onMouseUp, false);
+    canvas.addEventListener("touchcancel", onMouseUp, false);
+    canvas.addEventListener("touchmove", throttle(onMouseMove, 10), false);
+
+    for (var i = 0; i < colors.length; i++) {
+      colors[i].addEventListener("click", onColorUpdate, false);
+    }
+
     socketRef.current.on("drawing", onDrawingEvent);
-    setCanvas(document.getElementById("whiteboard")[0]);
-    console.log("set canvas", document.getElementById("whiteboard")[0]);
-    setColors(document.getElementsByClassName("color"));
-  }, []);
-  useEffect(() => {
-    console.log("canvas", canvas);
-    console.log(canvas);
-    if (canvas && firstLoad) {
-      setContext(canvas.getContext("2d"));
-      canvas.addEventListener("mousedown", onMouseDown, false);
-      canvas.addEventListener("mouseup", onMouseUp, false);
-      canvas.addEventListener("mouseout", onMouseUp, false);
-      canvas.addEventListener("mousemove", throttle(onMouseMove, 10), false);
 
-      //Touch support for mobile devices
-      canvas.addEventListener("touchstart", onMouseDown, false);
-      canvas.addEventListener("touchend", onMouseUp, false);
-      canvas.addEventListener("touchcancel", onMouseUp, false);
-      canvas.addEventListener("touchmove", throttle(onMouseMove, 10), false);
-      setFirstLoad(false);
-    }
-    return () => {
-      if (canvas) {
-        canvas.removeEventListener("mousedown", onMouseDown, false);
-        canvas.removeEventListener("mouseup", onMouseUp, false);
-        canvas.removeEventListener("mouseout", onMouseUp, false);
-        canvas.removeEventListener(
-          "mousemove",
-          throttle(onMouseMove, 10),
-          false
-        );
+    window.addEventListener("resize", onResize, false);
+    onResize();
 
-        //Touch support for mobile devices
-        canvas.removeEventListener("touchstart", onMouseDown, false);
-        canvas.removeEventListener("touchend", onMouseUp, false);
-        canvas.removeEventListener("touchcancel", onMouseUp, false);
-        canvas.removeEventListener(
-          "touchmove",
-          throttle(onMouseMove, 10),
-          false
-        );
+    function drawLine(x0, y0, x1, y1, color, emit) {
+      ctx.beginPath();
+      ctx.moveTo(x0, y0);
+      ctx.lineTo(x1, y1);
+      ctx.strokeStyle = color;
+      ctx.lineWidth = 2;
+      ctx.stroke();
+      ctx.closePath();
+
+      if (!emit) {
+        return;
       }
-    };
-  }, [canvas]);
-  useEffect(() => {
-    console.log(colors);
-    if (colors) {
-      for (var i = 0; i < colors?.length; i++) {
-        colors[i].addEventListener("click", onColorUpdate, false);
+      var w = canvas.width;
+      var h = canvas.height;
+
+      socketRef.current.emit("drawing", {
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color: color,
+      });
+    }
+
+    function onMouseDown(e) {
+      drawing = true;
+      current.x = e.clientX || e.touches[0].clientX;
+      current.y = e.clientY || e.touches[0].clientY;
+    }
+
+    function onMouseUp(e) {
+      if (!drawing) {
+        return;
       }
+      drawing = false;
+      drawLine(
+        current.x,
+        current.y,
+        e.clientX || e.touches[0].clientX,
+        e.clientY || e.touches[0].clientY,
+        current.color,
+        true
+      );
     }
-    return () => {
-      for (var i = 0; i < colors?.length; i++) {
-        colors[i].removeEventListener("click", onColorUpdate, false);
+
+    function onMouseMove(e) {
+      if (!drawing) {
+        return;
       }
-    };
-  }, [colors]);
-
-  const onMouseDown = (e) => {
-    console.log(e);
-    setDrawing(true);
-    setCurrent({
-      ...current,
-      x: e.clientX || e.touches[0].clientX,
-      y: e.clientY || e.touches[0].clientY,
-    });
-  };
-
-  const onMouseUp = (e) => {
-    if (!drawing) {
-      return;
+      drawLine(
+        current.x,
+        current.y,
+        e.clientX || e.touches[0].clientX,
+        e.clientY || e.touches[0].clientY,
+        current.color,
+        true
+      );
+      current.x = e.clientX || e.touches[0].clientX;
+      current.y = e.clientY || e.touches[0].clientY;
     }
-    setDrawing(false);
-    drawLine(
-      current.x,
-      current.y,
-      e.clientX || e.touches[0].clientX,
-      e.clientY || e.touches[0].clientY,
-      current.color,
-      true
-    );
-  };
 
-  const onMouseMove = (e) => {
-    console.log("onmousemove", drawing);
-    if (!drawing) {
-      return;
+    function onColorUpdate(e) {
+      current.color = e.target.className.split(" ")[1];
     }
-    console.log(e);
-    drawLine(
-      current.x,
-      current.y,
-      e.clientX || e.touches[0].clientX,
-      e.clientY || e.touches[0].clientY,
-      current.color,
-      true
-    );
-    setCurrent({
-      ...current,
-      x: e.clientX || e.touches[0].clientX,
-      y: e.clientY || e.touches[0].clientY,
-    });
-  };
-  const onColorUpdate = (e) => {
-    current.color = e.target.className.split(" ")[1];
-  };
 
-  // limit the number of events per second
-  const throttle = (callback, delay) => {
-    var previousCall = new Date().getTime();
-    return function () {
-      var time = new Date().getTime();
+    // limit the number of events per second
+    function throttle(callback, delay) {
+      var previousCall = new Date().getTime();
+      return function () {
+        var time = new Date().getTime();
 
-      if (time - previousCall >= delay) {
-        previousCall = time;
-        callback.apply(null, arguments);
-      }
-    };
-  };
-  const onDrawingEvent = (data) => {
-    var w = canvas.width;
-    var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-  };
-  const drawLine = (x0, y0, x1, y1, color, emit) => {
-    console.log(x0, y0, x1, y1, color, emit);
-    console.log(context);
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.strokeStyle = color;
-    context.lineWidth = 2;
-    context.stroke();
-    context.closePath();
-
-    if (!emit) {
-      return;
+        if (time - previousCall >= delay) {
+          previousCall = time;
+          callback.apply(null, arguments);
+        }
+      };
     }
-    var w = canvas.width;
-    var h = canvas.height;
 
-    socketRef.current.emit("drawing", {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color,
-    });
-  };
+    function onDrawingEvent(data) {
+      var w = canvas.width;
+      var h = canvas.height;
+      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    }
 
-  // make the canvas fill its parent
-  const onResize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  };
+    // make the canvas fill its parent
+    function onResize() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+  });
 
   return (
     <section aria-labelledby="notes-title" className="">
