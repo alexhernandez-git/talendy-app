8929a7d295f1a08b1e3c6ff3abe6340a671cd164
Alex Hernandez <vlexhndz@gmail.com>
2021-05-12 21:12:53 +0200

    fewafawe

--- pages/contribute-room/[room].js
+++ pages/contribute-room/[room].js
@@ -28,6 +28,7 @@ import moment from "moment";
 import { addRoomMessage, fetchRoomMessages } from "redux/actions/roomMessages";
 import { setEndOfContenteditable } from "helpers";
 import Finalize from "components/pages/ContributeRoom/Finalize";
+import ContributeExcalidraw from "components/Pages/Contribute/ContributeExcalidraw";
 const Audio = (props) => {
   const ref = useRef();
   useEffect(() => {
@@ -528,6 +529,26 @@ const Contribute = () => {
                   <div className="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl flex justify-between items-center ">
                     <div className="flex justify-center md:mt-0 flex-col-reverse justify-stretch space-y-4 space-y-reverse md:flex-row-reverse md:justify-end md:space-x-reverse md:space-y-0 md:space-x-3  md:space-x-3">
                       <button
+                        onClick={handleChangeFeature.bind(this, "EXCALIDRAW")}
+                        type="button"
+                        className="cursor-pointer inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5 mr-2"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                          <path
+                            fillRule="evenodd"
+                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        Shared screen
+                      </button>
+                      <button
                         onClick={handleChangeFeature.bind(this, "SHAREDNOTES")}
                         type="button"
                         className="cursor-pointer inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50"
@@ -660,6 +681,8 @@ const Contribute = () => {
                         sharedNotesRef={sharedNotesRef}
                       />
                     )}
+                  {feature.toUpperCase() === "EXCALIDRAW" &&
+                    contribute_room && <ContributeExcalidraw />}
                   {feature.toUpperCase() === "ASTEROIDS" && contribute_room && (
                     <ContributeAsteroids />
                   )}
