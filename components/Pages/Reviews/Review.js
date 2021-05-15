734c3920b7788aada62ef972fe4f64fa38986c9a
Alex Hernandez <vlexhndz@gmail.com>
2021-05-15 23:42:11 +0200

    wfeae

--- components/Pages/Reviews/Review.js
+++ components/Pages/Reviews/Review.js
@@ -9,11 +9,29 @@ const Review = ({ review }) => {
       <div className="relative py-4">
         <div className="relative flex items-start space-x-3">
           <div className="relative">
-            <img
-              className="h-10 w-10 rounded-full flex items-center justify-center"
-              src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-              alt=""
-            />
+            {review?.review_user?.picture ? (
+              <img
+                className="h-10 w-10 rounded-full flex items-center justify-center"
+                src={
+                  new RegExp(
+                    `${process.env.HOST}|https://freelanium.s3.amazonaws.com`
+                  ).test(review?.review_user?.picture)
+                    ? review?.review_user?.picture
+                    : process.env.HOST + review?.review_user?.picture
+                }
+                alt=""
+              ></img>
+            ) : (
+              <span className="bg-gray-100 rounded-full overflow-hidden h-10 w-10">
+                <svg
+                  className="text-gray-300 bg-gray-100 rounded-full h-10 w-10"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+              </span>
+            )}
           </div>
           <div className="min-w-0 flex-1">
             <div>
