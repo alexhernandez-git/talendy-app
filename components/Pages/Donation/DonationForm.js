ff9797e267cbc425e59400127ff2fd1cd2d69ec1
Alex Hernandez <vlexhndz@gmail.com>
2021-05-17 20:45:24 +0200

    wefafwe

--- components/Pages/Donation/DonationForm.js
+++ components/Pages/Donation/DonationForm.js
@@ -420,7 +420,23 @@ const DonationForm = () => {
                             onClick={handleOpenNewPaymentMethod}
                             className={`relative border rounded-bl-md rounded-br-md flex w-full justify-center`}
                           >
-                            <span className="p-2">Add new</span>
+                            <span className="p-2 flex justify-center items-center">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 mr-2"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={2}
+                                  d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                                />
+                              </svg>
+                              Add new
+                            </span>
                           </button>
                         </li>
                       </ul>
