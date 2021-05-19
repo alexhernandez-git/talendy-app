c8ec2d40c84e07be653afcee0595647c647cf696
Alex Hernandez <vlexhndz@gmail.com>
2021-05-19 12:07:40 +0200

    fewafwe

--- pages/index.js
+++ pages/index.js
@@ -147,6 +147,18 @@ export default function Home() {
     dispatch(changeCommunityFilter(selectedCommunity));
     router.push("/search");
   };
+  const [collaborateRoomImg, setCollaborateRoomImg] = useState(
+    "/static/images/screenshot-white-clean-cropped.png"
+  );
+  useEffect(() => {
+    if (authReducer.theme === "dark") {
+      setCollaborateRoomImg("/static/images/screenshot-dark-clean-cropped.png");
+    } else {
+      setCollaborateRoomImg(
+        "/static/images/screenshot-white-clean-cropped.png"
+      );
+    }
+  }, [authReducer.theme]);
   return (
     <Layout>
       <main className="mt-10">
@@ -347,7 +359,7 @@ export default function Home() {
                 <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
                   <img
                     className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                    src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                    src={collaborateRoomImg}
                     alt="Customer profile user interface"
                   />
                 </div>
