283e73cf6a6161da3fe115e924a9a64a39040dcb
Alex Hernandez <vlexhndz@gmail.com>
2021-05-21 20:28:12 +0200

    afee

--- components/Pages/Donation/DonationForm.js
+++ components/Pages/Donation/DonationForm.js
@@ -795,6 +795,7 @@ const DonationForm = () => {
           <div className="mt-2 flex justify-center">
             <span className="text-xs text-gray-400 dark:text-gray-300">
               20% of what you donate will go to the maintenance of this platform
+              so be generous
             </span>
           </div>
         </div>
