b662a194725f89c177753ab1948f9c10a3f768e6
Alex Hernandez <vlexhndz@gmail.com>
2021-05-24 13:50:29 +0200

    wfeaef

---
+++ pages/privacy.js
@@ -0,0 +1,215 @@
+import React from "react";
+
+const privacy = () => {
+  return (
+    <div>
+      <div className="relative py-16 bg-white  dark:bg-gray-800 overflow-hidden">
+        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+          <div
+            className="relative h-full text-lg max-w-prose mx-auto"
+            aria-hidden="true"
+          >
+            <svg
+              className="absolute top-12 left-full transform translate-x-32"
+              width="404"
+              height="384"
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    className="text-gray-200 dark:text-gray-700"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="384"
+                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+              />
+            </svg>
+            <svg
+              className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+              width="404"
+              height="384"
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    className="text-gray-200 dark:text-gray-700"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="384"
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              />
+            </svg>
+            <svg
+              className="absolute bottom-12 left-full transform translate-x-32"
+              width="404"
+              height="384"
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    className="text-gray-200 dark:text-gray-700"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="384"
+                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto">
+            <h1>
+              {/* <span className="block text-base text-center text-cyan-600 font-semibold tracking-wide uppercase">
+                Privacy Policy
+              </span> */}
+              <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+                Privacy Policy
+              </span>
+            </h1>
+          </div>
+          <div className="mt-6 prose prose-cyan prose-lg text-gray-500 dark:text-gray-200 mx-auto">
+            <p>
+              This Privacy Policy establishes the terms in which Talendy S.L.
+              uses and protects the information that is provided by its users
+              when using its website. This company is committed to the security
+              of its users' data. When we ask you to fill in the fields of
+              personal information with which you can be identified, we do so
+              ensuring that it will only be used in accordance with the terms of
+              this document. However, this Privacy Policy may change over time
+              or be updated, so we recommend and emphasize that you continually
+              review this page to ensure that you agree with such changes.
+            </p>
+
+            <h2 className="text-gray-900 dark:text-white">
+              Information that is collected
+            </h2>
+            <p>
+              Our website may collect personal information such as: Name,
+              contact information such as your email address and demographic
+              information. Likewise, when necessary, specific information may be
+              required to process an order or make a delivery or billing.
+            </p>
+            <h2 className="text-gray-900 dark:text-white">
+              Use of collected information
+            </h2>
+            <p>
+              Our website uses the information in order to provide the best
+              possible service, particularly to maintain a register of users,
+              orders if applicable, and improve our products and services. It is
+              possible that emails will be sent periodically through our site
+              with special offers, new products and other advertising
+              information that we consider relevant to you or that may provide
+              you with some benefit, these emails will be sent to the address
+              you provide and may be canceled. anytime. Talendy S.L. is highly
+              committed to fulfilling the commitment to keep your information
+              secure. We use the most advanced systems and constantly update
+              them to ensure that there is no unauthorized access.
+            </p>
+            <h2 className="text-gray-900 dark:text-white">Cookies</h2>
+            <p>
+              A cookie refers to a file that is sent in order to request
+              permission to be stored on your computer, by accepting said file
+              it is created and the cookie then serves to have information
+              regarding web traffic, and also facilitates future visits to a web
+              recurrent. Another function that cookies have is that with them
+              the web can recognize you individually and therefore provide you
+              with the best personalized service on its web. Our website uses
+              cookies to be able to identify the pages that are visited and
+              their frequency. This information is used only for statistical
+              analysis and then the information is permanently deleted. You can
+              delete cookies at any time from your computer. However, cookies
+              help to provide a better service on the websites, you do not give
+              access to information from your computer or from you, unless you
+              want it and provide it directly. You can accept or deny the use of
+              cookies, however most browsers automatically accept cookies as it
+              serves to have a better web service. You can also change your
+              computer settings to decline cookies. If they are declined, you
+              may not be able to use some of our services.
+            </p>
+            <h2 className="text-gray-900 dark:text-white">
+              Links to Third Parties
+            </h2>
+            <p>
+              This website may contain links to other sites that may be of
+              interest to you. Once you click on these links and leave our page,
+              we no longer have control over the site to which you are
+              redirected and therefore we are not responsible for the terms or
+              privacy or the protection of your data on those other third party
+              sites. These sites are subject to their own privacy policies, so
+              it is recommended that you consult them to confirm that you agree
+              with them.
+            </p>
+            <h2 className="text-gray-900 dark:text-white">
+              Control of your personal information
+            </h2>
+            <p>
+              At any time you can restrict the collection or use of personal
+              information that is provided to our website. Each time you are
+              asked to fill in a form, such as user registration, you can check
+              or uncheck the option to receive information by email. In case you
+              have marked the option to receive our newsletter or advertising,
+              you can cancel it at any time. This company will not sell,
+              transfer or distribute the personal information that is collected
+              without your consent, unless required by a judge with a court
+              order. Talendy S.L. It reserves the right to change the terms of
+              this Privacy Policy at any time
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default privacy;
