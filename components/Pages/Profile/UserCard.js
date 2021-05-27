8323c1cf025fce184d58741f236ca3fe8f3bfa19
Alex Hernandez <vlexhndz@gmail.com>
2021-05-27 21:02:51 +0200

    awffewa

--- components/Pages/Profile/UserCard.js
+++ components/Pages/Profile/UserCard.js
@@ -195,7 +195,7 @@ const UserCard = ({ mobile, page, profile, user }) => {
                           <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
                         </svg>
                         <span className="font-medium text-gray-700 ">
-                          Share your profile
+                          Share your wall
                         </span>
                       </button>
                     </span>
