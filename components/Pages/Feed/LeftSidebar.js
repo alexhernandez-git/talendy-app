6058bf4f9e83ad8d2f105c4b4b68dc54afb28185
Alex Hernandez <vlexhndz@gmail.com>
2021-05-28 18:37:14 +0200

    wefafe

--- components/Pages/Feed/LeftSidebar.js
+++ components/Pages/Feed/LeftSidebar.js
@@ -63,7 +63,28 @@ const LeftSidebar = ({ page }) => {
                   : "text-gray-600 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-800"
               } cursor-pointer group flex items-center justify-between px-3 py-2 text-sm font-medium rounded-3xl hover:text-gray-900`}
               >
-                <span className="truncate">Most Karma</span>
+                <span className="truncate flex items-center">
+                  Most{" "}
+                  <span
+                    className={`text-center text-orange-500 font-bold inline-flex items-center ml-1`}
+                  >
+                    <svg
+                      className="w-5 h-5 mr-0.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    Karma
+                  </span>
+                </span>
               </span>
             </Link>
             <Link href="/nearest">
