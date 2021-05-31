0bc4f327ef5166b13c53e735ed87111b9c3bae9b
Alex Hernandez <vlexhndz@gmail.com>
2021-05-31 18:17:13 +0200

    awfeewf

--- components/Pages/Contribute/ContributeKanbanBoard/List.js
+++ components/Pages/Contribute/ContributeKanbanBoard/List.js
@@ -8,18 +8,55 @@ const List = ({ title, cards, listID, index }) => {
     <Draggable draggableId={String(listID)} index={index}>
       {(provided) => (
         <div
-          style={{ backgroundColor: "#dfe3e6", minWidth: "18rem" }}
-          className="rounded-md p-3 mr-3 h-full bg-gray-200 dark:bg-gray-700 shadow"
+          style={{
+            backgroundColor: "#dfe3e6",
+            minWidth: "18rem",
+            maxWidth: "18rem",
+          }}
+          className="rounded-md p-3 mr-3 h-full bg-gray-200 dark:bg-gray-700 shadow group "
           {...provided.draggableProps}
           ref={provided.innerRef}
           {...provided.dragHandleProps}
         >
           <Droppable droppableId={String(listID)}>
             {(provided) => (
-              <div style={{ minWidth: "18rem" }}>
-                <h4 className="p-4 text-gray-900 dark:text-white truncate">
-                  {title}
-                </h4>
+              <div style={{ minWidth: "18rem", maxWidth: "18rem" }}>
+                <div className="flex justify-between">
+                  <h4 className="p-4 text-gray-900 dark:text-white truncate">
+                    {title}fewafewaaweffaweawefeawawfefeawaefwfaew
+                  </h4>
+                  <div className="text-gray-400 dark:text-gray-600 group-hover:text-gray-600 dark:group-hover:text-gray-200 transition flex items-center">
+                    <span className="cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                        <path
+                          fillRule="evenodd"
+                          d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    <span className="cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
                 <div {...provided.droppableProps} ref={provided.innerRef}>
                   {cards.map((card, index) => (
                     <Card
