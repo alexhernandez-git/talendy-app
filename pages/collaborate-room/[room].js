835ce21247238b4134ae25c118c10ef6f3f62595
Alex Hernandez <vlexhndz@gmail.com>
2021-06-01 13:51:11 +0200

    fewafe

--- pages/collaborate-room/[room].js
+++ pages/collaborate-room/[room].js
@@ -565,7 +565,7 @@ const Contribute = () => {
                     >
                       <path d="M2 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H3a1 1 0 01-1-1V4zM8 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H9a1 1 0 01-1-1V4zM15 3a1 1 0 00-1 1v12a1 1 0 001 1h2a1 1 0 001-1V4a1 1 0 00-1-1h-2z" />
                     </svg>
-                    Kanban board <span className="text-red-500 ml-2">Beta</span>
+                    Kanban board
                   </button>
                   <button
                     onClick={handleChangeFeature.bind(this, "SHAREDWHITEBOARD")}
