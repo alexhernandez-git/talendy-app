c3b0ab4a606171dee2c30122224d536de697537c
Alex Hernandez <vlexhndz@gmail.com>
2021-06-07 20:44:28 +0200

    erfafwse

--- pages/index.js
+++ pages/index.js
@@ -206,7 +206,22 @@ export default function Home() {
                     </span>
                     <Link href={"/feed"}>
                       <span className="cursor-pointer flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r hover:opacity-90 from-orange-500 to-pink-500 hover:to-pink-600 sm:px-8">
-                        See posts
+                        Earn{" "}
+                        <svg
+                          className="w-5 h-5 ml-1 mr-0.5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>{" "}
+                        Karma
                       </span>
                     </Link>
                   </div>
