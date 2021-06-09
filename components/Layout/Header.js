2d6dc815c4c312043bbfce24a13b9969bc123b2f
Alex Hernandez <vlexhndz@gmail.com>
2021-06-09 21:24:13 +0200

    weaffew

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -155,7 +155,7 @@ const Header = ({ handleOpenModal, page, handleToggleRegister }) => {
     <header className="bg-white dark:bg-gray-700 shadow-sm lg:sticky lg:overflow-y-visible top-0 z-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 static">
         <div className="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
-          <div className="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
+          <div className="flex xl:col-span-2">
             <div className="flex-shrink-0 flex items-center">
               <Link href={authReducer.is_authenticated ? "/feed" : "/"}>
                 <div>
@@ -261,11 +261,6 @@ const Header = ({ handleOpenModal, page, handleToggleRegister }) => {
           </div>
           {authReducer.is_authenticated ? (
             <div className="hidden lg:flex lg:items-center lg:justify-end xl:col-span-4">
-              <Link href="/dashboard">
-                <span className="cursor-pointer ml-6 inline-flex items-center px-4 py-2 text-sm font-medium rounded-3xl shadow-sm text-white bg-gradient-to-r from-orange-500 to-pink-500 hover:to-pink-600">
-                  Dashbaord
-                </span>
-              </Link>
               <span
                 onClick={handleOpenChats}
                 className={`ml-5 cursor-pointer flex-shrink-0 bg-white  rounded-full p-1  dark:text-white  dark:hover:text-gray-200 ${
@@ -418,9 +413,14 @@ const Header = ({ handleOpenModal, page, handleToggleRegister }) => {
                   )}
                 </div>
               </div>
+              <Link href="/dashboard">
+                <span className="ml-5 cursor-pointer inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 hover:text-gray-500 dark:hover:text-white dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50">
+                  Dashbaord
+                </span>
+              </Link>
               <span
                 onClick={handleOpenModal}
-                className="cursor-pointer ml-6 inline-flex items-center px-4 py-2 text-sm font-medium rounded-3xl shadow-sm text-white bg-gradient-to-r from-orange-500 to-pink-500 hover:to-pink-600"
+                className="cursor-pointer ml-5 inline-flex items-center px-4 py-2 text-sm font-medium rounded-3xl shadow-sm text-white bg-gradient-to-r from-orange-500 to-pink-500 hover:to-pink-600"
               >
                 Post
               </span>
