5fc98a1abf0485739eb5a87b83ad104af47ce593
Alex Hernandez <vlexhndz@gmail.com>
2021-06-21 17:04:39 +0200

    fewaafwe

--- pages/dashboard/posts.js
+++ pages/dashboard/posts.js
@@ -32,117 +32,122 @@ const posts = () => {
               </button>
             </div>
 
-            {/* Start users lists */}
-            <div className="bg-white shadow overflow-hidden sm:rounded-md mt-3">
-              <ul className="divide-y divide-gray-200">
-                <li>
-                  <a
-                    target="_blank"
-                    href="/user/122"
-                    className="block hover:bg-gray-50"
-                  >
-                    <div className="flex items-center px-4 py-4 sm:px-6">
-                      <div className="min-w-0 flex-1 flex items-center">
-                        <div className="min-w-0 flex-1 pr-4 md:grid md:grid-cols-2 md:gap-4">
-                          <div>
-                            <p className="text-sm font-medium text-orange-600 truncate">
-                              Post ttitleaiffeaw Lorem ipsum dolor sit amet
-                              conecessitatibus alias dolores eligendi odit
-                              nostrum?
-                            </p>
-                            <p className="mt-2 flex items-center text-sm text-gray-500">
+            {/* Start users table */}
+            <div className="flex flex-col ">
+              <div className="overflow-x-auto">
+                <div className="py-2 align-middle inline-block min-w-full">
+                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="bg-gray-100">
+                        <tr>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Name
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Admin
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Members
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Status
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider "
+                          >
+                            <span className="sr-only">See post</span>
+                          </th>
+                          {/* 
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Role
+                          </th> */}
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white divide-y divide-gray-200">
+                        {/* User item */}
+                        <tr>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <span className="text-sm">afewafewfe</span>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <span className="text-sm cursor-pointer text-gray-500 hover:text-gray-600">
+                              Alex Hernandez
+                            </span>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <span className="text-sm">3</span>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                              Active
+                            </span>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <a href="/post/3443" target="_blank">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                className="h-5 w-5 text-gray-500"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
                               >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                                  clipRule="evenodd"
-                                />
+                                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
                               </svg>
-                              <span className="truncate">Ricardo cooper</span>
-                            </p>
-                          </div>
-                          <div className="hidden md:block">
-                            <div>
-                              <p className="text-sm text-gray-900">
-                                Created on{" "}
-                                <time datetime="2020-01-07">
-                                  January 7, 2020
-                                </time>
-                              </p>
-                              <p className="mt-2 flex items-center text-sm text-gray-500">
-                                <svg
-                                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                                  aria-hidden="true"
-                                >
-                                  <path
-                                    fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                    clip-rule="evenodd"
-                                  />
-                                </svg>
-                                Active
-                              </p>
-                            </div>
-                          </div>
-                        </div>
+                            </a>
+                          </td>
+                        </tr>
+                        {/* End user item */}
+                      </tbody>
+                    </table>
+                    {/* Pagination */}
+                    <nav
+                      className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+                      aria-label="Pagination"
+                    >
+                      <div className="hidden sm:block">
+                        <p className="text-sm text-gray-700">
+                          Showing <span className="font-medium">1</span> to{" "}
+                          <span className="font-medium">10</span> of{" "}
+                          <span className="font-medium">20</span> results
+                        </p>
                       </div>
-                      <div>
-                        <svg
-                          className="h-5 w-5 text-gray-400"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          aria-hidden="true"
+                      <div className="flex-1 flex justify-between sm:justify-end">
+                        <a
+                          href="#"
+                          className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 hover:text-gray-500 dark:hover:text-white dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50"
                         >
-                          <path
-                            fill-rule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                          Previous
+                        </a>
+                        <a
+                          href="#"
+                          className="ml-3 inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 hover:text-gray-500 dark:hover:text-white dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50"
+                        >
+                          Next
+                        </a>
                       </div>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-              {/* Pagination */}
-              <nav
-                className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
-                aria-label="Pagination"
-              >
-                <div className="hidden sm:block">
-                  <p className="text-sm text-gray-700">
-                    Showing <span className="font-medium">1</span> to{" "}
-                    <span className="font-medium">10</span> of{" "}
-                    <span className="font-medium">20</span> results
-                  </p>
+                    </nav>
+                    {/* End pagination */}
+                  </div>
                 </div>
-                <div className="flex-1 flex justify-between sm:justify-end">
-                  <a
-                    href="#"
-                    className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 hover:text-gray-500 dark:hover:text-white dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50"
-                  >
-                    Previous
-                  </a>
-                  <a
-                    href="#"
-                    className="ml-3 inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-3xl text-gray-500 hover:text-gray-500 dark:hover:text-white dark:text-white bg-white dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-50"
-                  >
-                    Next
-                  </a>
-                </div>
-              </nav>
-              {/* End pagination */}
+              </div>
             </div>
-            {/* End users lists */}
+            {/* End users table */}
           </div>
         </div>
       </main>
